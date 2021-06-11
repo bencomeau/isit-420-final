7e83f76904527c06739061df8fda11f01737e4d1
xbt23x <47095043+xbt23x@users.noreply.github.com>
2021-06-11 14:18:38 -0700

    pushing changes made to use remote class

---
+++ ISIT420HomeworkFour/Scripts/final.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener("DOMContentLoaded", function (event) {
+
+    document.getElementById("getRace").addEventListener("click", function () {
+
+        //getRaceData1();
+       // getRaceData2();
+       // getRaceData3();
+    });
+
+});
+
+function getUnemployment() {
+
+    $.getJSON("api/order/query1")
+        .done(function (data) {
+            //$.each(data, function (key, item) {
+            //    // Add a list item for the product.
+            //    $('<li>', { text: formatItem1(item) }).appendTo($('#displayRace1'));
+            //});
+            //console.log(data);
+            let table = document.getElementById("table1");
+            generateTable(table, data);
+        });
+}
